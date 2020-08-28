74246822ad59fdc09e3f86441f54532eaa401d1d
Christian Ortega <christianmortega7@gmail.com>
2020-08-27 22:00:20 -0400

    Implement iterative fibonacci method

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,9 +1,18 @@
 # Iterative solution
 def fibs(n)
-  
+  return [] if n <= 0
+  return [0] if n == 1
+  return [0, 1] if n == 2
+  fib_array = [0, 1]
+  2.upto(n - 1) do |i|
+    fib_array.push(fib_array[i - 1] + fib_array[i - 2])
+  end
+  fib_array
 end
 
 # Recursive solution
 def fibs_rec
   
 end
+
+p fibs(12)
