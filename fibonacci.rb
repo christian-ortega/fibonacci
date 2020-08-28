8c587805eded026f011e93dad3afc529710a9fe1
Christian Ortega <christianmortega7@gmail.com>
2020-08-27 22:14:06 -0400

    Implement recursive fibonacci method

--- fibonacci.rb
+++ fibonacci.rb
@@ -11,8 +11,13 @@ def fibs(n)
 end
 
 # Recursive solution
-def fibs_rec
-  
+def fibs_rec(n)
+  return [] if n <= 0
+  return [0] if n == 1
+  return [0, 1] if n == 2
+  temp_array = fibs_rec(n - 1)
+  fib_array = temp_array.push(temp_array[n - 2] + temp_array[n - 3])
 end
 
 p fibs(12)
+p fibs_rec(12)
